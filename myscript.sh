d3c720b4651b297da1906edd114c09b45cd7b3c8
Phillip Kataswa <65820314+Artsia@users.noreply.github.com>
2023-06-03 13:12:49 -0400

    commit

--- myscript.sh
+++ myscript.sh
@@ -1,3 +1,25 @@
+
+
+
+# create .sh file in /home/ec2-user directory with touch command
+
+#Next edit .sh file with  nano myscript.sh
+#copy and paste code below into .sh file, save and go back into /home/ec2-user directory
+#excute .sh file - bash myscript.sh
+#after execution of file, there should be a newly created directory which has a .sh file inside it. 
+# Use ls command and cd into directory to see contents
+
+
+
+
+#code bellow will create directory and file.
+# Write data into files
+#manipulate permissions and verify directory and file creation
+
+
+
+
+
 echo "Current directory"
 
 pwd # Print the working directory
@@ -29,11 +51,17 @@ then
 else
     touch script.sh # Create the "script.sh" file
     echo "File '$file' created."
-fi
+fi #end condition
 
-echo "Show the directories and files"
+echo "Show working directoty"
+
+pwd
+
+
+echo "show files and directories in the $directory that was just created"
 
 dir # Show the directories and files in the current folder (optional command, not necessary)
+
 
 fileData="script.sh" # Update the file variable to "script.sh" (no need for the full path)
 
@@ -54,7 +82,87 @@ done
 echo "The sum of numbers is: $sum"
 ' > "$fileData" # Use single quotes around the script content to preserve the newlines
 
-dir
+echo "show contents of the data in $fileData with cat command"
 
-bash script.sh
+cat $fileData
+
+echo "current directory"
+
+pwd
+
+echo "test if file exists in this dierectory: $directory"
+
+# returns an exit status of 0 (true) or 1 (false) 
+if [ -e $fileData ]; then
+    echo "File exists"
+else
+    echo "File does not exist"
+fi #end condition
+
+# show file permission for file
+
+ls $fileData -l
+
+#remove write permission for everyone
+chmod 440 $fileData
+
+echo "After removing write permission, we wont be able to write to file"
+ls $fileData -l
+
+#verify by using nano 
+
+nano $fileData
+
+echo "now i add the write permision back"
+
+chmod 666 $fileData
+ls $fileData -l
+
+nano $fileData
+
+#Grant write access to users who are allowed
+
+
+#change user/owner of file
+
+#create new users and assign new groups to each uears if needed
+
+
+#Create usernames and passwords for each user
+
+
+
+# Assign permission to users o access the fileData file
+
+
+
+
+#Show that only these users can access that file
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
